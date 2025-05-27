1b401cf28b8aba9edbeeddc92279b0cb1d4c0b02
yosibagus <yosibagusdsd@gmail.com>
2025-05-27 12:10:14 +0700

    event menus update

--- resources/views/user/layouts/menu_event.blade.php
+++ resources/views/user/layouts/menu_event.blade.php
@@ -40,31 +40,40 @@
             flex-wrap: wrap;
         }
     }
+
+    /* Default: tampilkan semua */
+    .hide-on-mobile {
+        display: inline;
+    }
+
+    /* Di layar kecil (mobile ≤ 700px), sembunyikan elemen ini */
+    @media (max-width: 700px) {
+        .hide-on-mobile {
+            display: none !important;
+        }
+    }
 </style>
 <nav class="navbar shadow-nav fixed-top bg-white">
     <div class="container-fluid">
         <div class="row justify-content-center w-100">
             <div class="col-12 col-xl-8">
                 <div class="d-flex align-items-center p-2 flex-wrap justify-content-between">
-
+                    
                     <!-- Logo -->
                     <a class="navbar-brand me-3" href="{{ url('/') }}">
                         <!-- Logo besar -->
-                        <img src="{{ asset('logo.png') }}" alt="Logo Besar" class="logo-desktop" width="120">
+                        <img src="{{ asset('logo.png') }}" alt="Logo Besar" class="logo-desktop" width="100">
                         <!-- Logo kecil -->
-                        <img src="{{ asset('logo_kecil.png') }}" alt="Logo Kecil" class="logo-mobile" width="60">
+                        <img src="{{ asset('logo_kecil.png') }}" alt="Logo Kecil" class="logo-mobile">
                     </a>
 
                     <!-- Menu -->
-                    <div class="menu-wrapper d-flex align-items-center flex-wrap justify-content-end" style="gap:16px;">
-                        <a class="nav-link px-2" href="javascript:void(0);"
-                            onclick="scrollToSection('deskripsi')">Deskripsi</a>
-                        <a class="nav-link px-2" href="javascript:void(0);"
-                            onclick="scrollToSection('leaderboard')">Leaderboard</a>
-                        <a class="nav-link px-2" href="javascript:void(0);"
-                            onclick="scrollToSection('finalis')">Finalis</a>
-                        <a class="nav-link px-2" href="javascript:void(0);"
-                            onclick="scrollToSection('pesan-masuk')">Dukungan</a>
+                    <div class="menu-wrapper d-flex align-items-center flex-nowrap justify-content-end" style="gap:16px;">
+                        <a class="nav-link px-2" href="javascript:void(0);" onclick="scrollToSection('deskripsi')">Deskripsi</a>
+                        <a class="nav-link px-2" href="javascript:void(0);" onclick="scrollToSection('leaderboard')">Leaderboard</a>
+                        <a class="nav-link px-2" href="javascript:void(0);" onclick="scrollToSection('finalis')">Finalis</a>
+                        <a class="nav-link px-2" href="javascript:void(0);" onclick="scrollToSection('pesan-masuk')">Pesan Voters</a>
+                        <!-- Tambahkan menu lain di sini jika perlu -->
                     </div>
 
                 </div>
